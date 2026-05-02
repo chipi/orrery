676610fda2a568578420cc79bd3c823e50727309
chipi <chipi@users.noreply.github.com>
2026-05-02 19:47:21 +0000

    deploy: bd95ca187c47baf1fa910fa95e45e25fb4bc2442

--- _app/immutable/chunks/stores.BNvR1ZgE.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./entry.BTS2WCbJ.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ _app/immutable/entry/start.BP3_QNU-.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Coc-1JVC.js";export{t as start};

5459d40ccd7b2aa5a6e6d1617ebe16da5eb16bb1
chipi <chipi@users.noreply.github.com>
2026-06-16 20:52:14 +0000

    deploy: 70295bd69de66ec96ef7114255598c6703e237e5

--- _app/immutable/entry/start.1zj0uLii.js
+++
@@ -1,1 +0,0 @@
-import{b as s}from"../chunks/BAef_Us1.js";import{Q as t}from"../chunks/C14gFEzW.js";export{t as load_css,s as start};
---
+++ _app/immutable/entry/start.BZkwFVXo.js
@@ -0,0 +1,1 @@
+import{b as s}from"../chunks/C6_XRoOn.js";import{Q as t}from"../chunks/CKBHsUd3.js";export{t as load_css,s as start};
